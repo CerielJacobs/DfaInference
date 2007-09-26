2baf8e2be3c91747b4885930f2d9ea0120819d28
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2007-09-26 11:24:46 +0000

    Added

---
+++ src/DfaInference/HighDepthStrategy.java
@@ -0,0 +1,31 @@
+package DfaInference;
+
+/**
+ * Implements the "highest depth" strategy for picking the next blue state
+ * to be dealt with.
+ */
+public class HighDepthStrategy implements PickBlueStrategy {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Determines the next blue to be dealt with. The parameter specifies
+     * the possibilities, both merges and promotions. From this list, this
+     * method chooses the blue state with the lowest depth.
+     * @param dfa the DFA.
+     * @param choices the merge and promotion possibilities.
+     * @return the state number of the blue state to be dealt with next.
+     */
+    public int getBlue(DFA dfa, Choice[] choices) {
+        int blue = -1;
+        int depth = -1;
+        for (int i = 0; i < choices.length; i++) {
+            State s = dfa.getState(choices[i].s2);
+            if (s.depth > depth) {
+                blue = choices[i].s2;
+                depth = s.depth;
+            }
+        }
+        return blue;
+    }
+}

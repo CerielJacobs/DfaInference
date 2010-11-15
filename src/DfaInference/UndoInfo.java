2fc203024da3a86755e6a75adb9db4986b6010c8
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2010-11-15 12:21:55 +0000

    Fix

--- src/DfaInference/UndoInfo.java
+++ src/DfaInference/UndoInfo.java
@@ -54,7 +54,7 @@ public final class UndoInfo implements Configuration {
             weight = s.getWeight();
             traffic = s.getTraffic();
             xTraffic = s.getxTraffic();
-            if (USE_CHISQUARE) {
+            if (USE_CHISQUARE || USE_COMPATIBILITY_CHECK) {
                 edgeWeights = s.edgeWeights.clone();
                 xEdgeWeights = s.xEdgeWeights.clone();
             }
